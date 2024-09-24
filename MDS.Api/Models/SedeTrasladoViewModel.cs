9155d73cdef79ef8f784cc6b462e2dd41e3b1abd
htorresanna <henrry.torres@sanna.pe>
2024-09-24 16:35:58 -0500

    Avances ambulancia 24 - 09

---
+++ MDS.Api/Models/SedeTrasladoViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MDS.Api.Models
+{
+    public class CreateSedeTrasladoViewModel
+    {
+        public string? ruc { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string referencia { get; set; }
+        public string cod_ubigeo { get; set; }
+        public int usuario_creacion { get; set; }
+    }
+
+    public class UpdateSedeTrasladoViewModel
+    {
+        public long id { get; set; }
+        public string? ruc { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string referencia { get; set; }
+        public string cod_ubigeo { get; set; }
+        public int usuario_modificacion { get; set; }
+    }
+}
---
+++ MDS.Services/SedeTraslado/ISedeTrasladoService.cs
@@ -0,0 +1,21 @@
+﻿using MDS.Dto;
+using MDS.Infrastructure.Helper;
+using MDS.Infrastructure.Services;
+
+namespace MDS.Services.SedeTraslado
+{
+    public interface ISedeTrasladoService : IService
+    {
+        //By Henrry Torres
+        Task<ServiceResponse> GetSedeTraslado();
+
+        //By Henrry Torres
+        Task<ServiceResponse> GetSedesTrasladoFiltro(string busqueda, string condicion);
+
+        //By Henrry Torres
+        Task<ServiceResponse> AddSedeTraslado(SedeTrasladoMtoDto dto);
+
+        //By Henrry Torres
+        Task<ServiceResponse> UpdateSedeTraslado(SedeTrasladoMtoDto dto);
+    }
+}
